d586c191cf2bb0544c49bf0ce6cbfd03bd49daa6
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 17:09:08 +0530

    Operator Precendance

---
+++ 0013_JavaScript(Part_1)/04_Operator_Precedance/01_Op.js
@@ -0,0 +1,7 @@
+// Precedance: 
+// If at same level then we go from left to right
+
+// () If at same level then we go from left to right
+// ** If at same level then we go from right to left
+// *,/,% If at same level then we go from left to right
+// +,- If at same level then we go from left to right
