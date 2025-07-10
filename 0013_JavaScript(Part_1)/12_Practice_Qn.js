8b8a73a662764fa2733fbce9d74d7be0ba546073
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 18:21:37 +0530

    Practice Qn

---
+++ 0013_JavaScript(Part_1)/12_Practice_Qn.js
@@ -0,0 +1,14 @@
+let name = "Kishan Agarwal"
+console.log(name.length);
+
+let fname = "Kishan"
+console.log(fname[0]);
+console.log(fname[fname.length - 1]);
+
+console.log("Kishan"+123);
+
+let str1 = "";
+let str2 = " ";
+
+console.log(str1.length); // 0
+console.log(str2.length); // 1
