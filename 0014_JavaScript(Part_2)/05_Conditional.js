9a4ed4858cdb92487236ef1eca2ba4cfd582c6cd
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 23:24:40 +0530

    Conditionals

---
+++ 0014_JavaScript(Part_2)/05_Conditional.js
@@ -0,0 +1,10 @@
+let age = 16;
+
+if (age > 18) {
+    console.log("You can drive");
+} else {
+    console.log("You cannot drive");
+}
+
+// else if --> if the if code and above else if codes fails
+// else --> if all the other conditions fails then else will ultimately run
