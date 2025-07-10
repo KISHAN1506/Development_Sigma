25331a23f3a08345ca5f2a33b336fb5daf3496b1
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 17:56:01 +0530

    Unary Operator

---
+++ 0013_JavaScript(Part_1)/07_Unary_Operator.js
@@ -0,0 +1,11 @@
+let year = 2000;
+
+year++;
+console.log(year);
+
+
+// a++ = Post Increment Operator 
+// ++a = Pre Increment Operator
+
+// a-- = Post Decrement Operator 
+// --a = Pre Decrement Operator
