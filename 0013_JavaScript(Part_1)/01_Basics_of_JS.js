5aef5483dc185821a8e7bec23c4bc723fdd7c08c
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 16:57:55 +0530

    Basics of JS

--- 0013_JavaScript(Part_1)/01_Basics_of_JS.js
+++ 0013_JavaScript(Part_1)/01_Basics_of_JS.js
@@ -1,1 +1,18 @@
 console.log(1+2);
+
+name="Kishan";
+
+a = 10;
+b = 5;
+
+console.log(a+b);
+console.log(name);
+
+console.log(typeof(name));
+
+
+// Datatype :
+// 
+// Numbers is a datatype which contains - integer, float
+// 
+// 
