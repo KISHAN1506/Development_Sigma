88466cbe1cfed07671754afdae8c75c987963b3e
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 18:17:24 +0530

    Null and Undefined

---
+++ 0013_JavaScript(Part_1)/11_Null_and_Undefined.js
@@ -0,0 +1,11 @@
+// null & undefined in JS
+
+// undefined
+// A variable that has not been assigned a value is of type undefined.
+
+// null
+// The null value represents the intentional absence of any object value.
+// To be explicitly assigned.
+
+let a = null;
+console.log(typeof(a));
