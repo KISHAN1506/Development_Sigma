b7946c87120db332aab1dea1117ab7a01f65f459
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 18:11:10 +0530

    String

---
+++ 0013_JavaScript(Part_1)/10_String.js
@@ -0,0 +1,12 @@
+let name = "Kishan";
+let name2 = 'Kishan';
+
+console.log(typeof(name));
+console.log(typeof(name2));
+
+
+let emp = "";
+console.log(typeof(emp));
+
+// In string double quote can contain single quote and vice versa 
+// but can't have same type of quote inside
