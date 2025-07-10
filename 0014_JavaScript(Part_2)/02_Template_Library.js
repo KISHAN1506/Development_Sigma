5c7e5459c95ca2ddc7ed1eb43aee9628327591f1
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 19:27:45 +0530

    Template Library

---
+++ 0014_JavaScript(Part_2)/02_Template_Library.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 6;
+
+// They are used to add the embedded expressions in a string
+// ` == backtick
+// let output = "Total of all is " + (a+b)+" units";
+
+let output = `Total of all is ${a+b} units`
+
+console.log(output);
