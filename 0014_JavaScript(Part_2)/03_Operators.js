3c9ecfaaceca064dd285ceeb8dd68a2d6bd7b2cb
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-10 23:24:08 +0530

    Operators

---
+++ 0014_JavaScript(Part_2)/03_Operators.js
@@ -0,0 +1,28 @@
+// a + b
+// a & b are operand and + is operator
+
+// Comparision Operator
+
+console.log(18 >= 17)
+
+// != --> not equal to operator
+
+console.log("Direct Comparision :")
+console.log(5 == "5");
+
+
+// for both type and comparision operator we use ===
+console.log("Proper Direct Comparision :")
+console.log(5 === "5");
+
+
+console.log("Zero Comparision :")
+console.log(0 == "");
+console.log(0 === "");
+
+
+// Strict comparision
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log(1 == true);
