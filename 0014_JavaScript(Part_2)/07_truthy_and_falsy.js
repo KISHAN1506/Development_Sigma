25a151e31d386db194c07390a4b2dce6a6968c2b
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-11 17:07:29 +0530

    Truthy and Falsy

---
+++ 0014_JavaScript(Part_2)/07_truthy_and_falsy.js
@@ -0,0 +1,18 @@
+if(true) {
+    console.log("true");
+} else {
+    console.log("Not true");
+}
+
+
+if(false) {
+    console.log("true");
+} else {
+    console.log("Not true");
+}
+
+// Falsy values
+// false, 0, -0, On (BigInt value), '' (empty string) , null, undefined, NaN
+
+// Truthy values
+// Everything else
