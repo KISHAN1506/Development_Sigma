5d23cc427e934679762614ca4a4527f36698b204
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-12 23:25:06 +0530

    Const in Array

---
+++ 0015_JavaScript(Part_3)/Const_Array.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3];
+
+// IN const in array , push pop everthing can be done
+// Just the array cannot be replaced by new array
+
+arr.push(4);
+console.log(arr);
+arr.pop();
+console.log(arr);
+
+
+// just this is not allowed , ie space address / reference cannot be changed
+// arr = [1, 2, 3]
