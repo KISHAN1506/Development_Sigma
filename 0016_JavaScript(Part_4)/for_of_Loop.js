8b6ec4eb3cabbd80486713feb1bfb786b179d318
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-14 22:11:53 +0530

    For of Loop

---
+++ 0016_JavaScript(Part_4)/for_of_Loop.js
@@ -0,0 +1,14 @@
+let names = ["kishan", "Khushi", "Rohit"]
+
+
+for(naam of names){
+    console.log(naam);
+}
+
+let heroes = [["ironman", "spiderman", "thor"], ["suparman", "wonder women", "flash"]]
+
+for (list of heroes){
+    for (names of list){
+        console.log(names);
+    }
+}
