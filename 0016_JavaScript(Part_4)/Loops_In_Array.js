077dcc32ebc5de8c61d5dca52b0986c8ed726835
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-14 22:07:44 +0530

    Loops In Array

---
+++ 0016_JavaScript(Part_4)/Loops_In_Array.js
@@ -0,0 +1,13 @@
+// let names = ["kishan", "Khushi", "Rohit"]
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+let heroes = [["ironman", "spiderman", "thor"], ["suparman", "wonder women", "flash"]]
+
+for (let i = 0; i <heroes.length;i++){
+    for (let j = 0; j <heroes[i].length;j++){
+        console.log(heroes[i][j]);
+    }
+}
