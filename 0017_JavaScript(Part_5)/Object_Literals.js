901e1366e03d8c8d0f72989d8a681e0125b3dfe0
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-17 19:47:09 +0530

    Object Literal

---
+++ 0017_JavaScript(Part_5)/Object_Literals.js
@@ -0,0 +1,22 @@
+// used to store keyed collection and complex entities
+
+
+// generally objects are made constant
+const person = {
+    name: "Kishan",
+    age: 19
+};
+const items = {
+    price: 99.99,
+    discount: 40,
+    colors: ['green', 'magenta']
+}
+
+const post = {
+    username : "@kishan",
+    content: "First Post",
+    likes: 100,
+    repost: 50
+};
+
+console.log(person);
