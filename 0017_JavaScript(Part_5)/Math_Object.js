82b4f9b9cbcfa71ef7ae61191164fa2b763a99cf
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-17 20:38:31 +0530

    Math Object

---
+++ 0017_JavaScript(Part_5)/Math_Object.js
@@ -0,0 +1,26 @@
+console.log(Math);
+
+// Absolute Value
+console.log(Math.abs(12));
+console.log(Math.abs(-12));
+
+
+// Power
+console.log(Math.pow(2,4));
+
+// Floor
+console.log(Math.floor(12.6));
+console.log(Math.floor(-12.6));
+
+// Ceiling
+console.log(Math.ceil(12.6));
+console.log(Math.ceil(-12.6));
+
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
