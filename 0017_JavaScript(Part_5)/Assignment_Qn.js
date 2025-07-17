40e8a17ec2d45c7df217e50062fac381deb3869b
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-17 20:38:10 +0530

    Assignment Qns

---
+++ 0017_JavaScript(Part_5)/Assignment_Qn.js
@@ -0,0 +1,27 @@
+// Q1 -> dice roll
+for (let i = 0; i < 10; i++) {
+    let random = Math.floor(Math.random() * 6) + 1;
+    console.log(random);
+}
+
+// Q2:
+
+let car = {
+    name: "BMW",
+    model: "M5",
+    color: "Black"
+};
+
+console.log(car.name);
+
+let person = {
+    name: "Kishan",
+    age: 19,
+    city: "Surat"
+};
+
+person.city = "New York";
+person.country = "US"
+
+console.log(person);
+
