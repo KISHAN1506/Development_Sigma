2d1e30fa78b0e9b28904909d087cf60b0b7f3ab4
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-17 20:14:06 +0530

    Array of objects

---
+++ Array_of_Objects.js
@@ -0,0 +1,19 @@
+const classInfo = [
+    {
+        name: "kishan",
+        grade: "A+",
+        city: "Delhi"
+    },
+    {
+        name: "rudra",
+        grade: "A",
+        city: "Delhi"
+    },
+    {
+        name: "deebu",
+        grade: "A",
+        city: "Delhi"
+    }
+]
+
+console.log(classInfo[1].name);
