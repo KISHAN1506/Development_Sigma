a3bad0541b11d7c6d8529029493ab858a94bb756
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-19 20:08:48 +0530

    Function Expression

---
+++ 0018_JavaScript(Part_6)/Function_Expression.js
@@ -0,0 +1,14 @@
+let sum = function (a, b) {
+    return a + b;
+}
+
+console.log(sum(1,2));
+
+
+let hello = function(){
+    console.log("Hello Ji!");
+}
+console.log(hello());
+console.log(hello());
+console.log(hello());
+
