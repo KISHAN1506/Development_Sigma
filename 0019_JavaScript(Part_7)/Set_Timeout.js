8e63107ff1aef7c7d7412e6f3e4b83527b55ed63
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-21 19:16:57 +0530

    Set Timeout

---
+++ 0019_JavaScript(Part_7)/Set_Timeout.js
@@ -0,0 +1,10 @@
+// setTimeout(function,timeout) 
+// --> timeout takes time in mili second. ; for 1s:1000ms
+
+console.log("hello Everyone");
+
+setTimeout(() => {
+    console.log("Kishan Agarwal");
+}, 4000);
+
+console.log("My name is");
