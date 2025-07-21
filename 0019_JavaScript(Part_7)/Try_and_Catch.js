1fcb81856b102a99a29e0be48725adee03315832
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-21 19:17:16 +0530

    try and catch

---
+++ 0019_JavaScript(Part_7)/Try_and_Catch.js
@@ -0,0 +1,17 @@
+// try & catch
+// The try statement allows you to define a block of code to be tested for errors while it is being executed.
+// The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+
+console.log("Hello");
+console.log("Hello");
+
+try {
+    console.log(a);
+} catch(err) {
+    console.log("a not defined");
+    console.log(err);
+}
+
+console.log("Hello2");
+console.log("Hello2");
+console.log("Hello2");
