7d978e624d187ca6dfca38513055a0fa290ed8fe
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-24 23:20:41 +0530

    Spread

---
+++ 0020_JavaScript(Part_8)/02_Spread.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3,4];
+let name = "Kishan Agarwal is my name"
+console.log(...arr);
+console.log(...name);
+
+let arr2 = [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4];
+console.log(Math.min(...arr2));
+console.log(Math.max(...arr2));
