8a12fcc5ab94b3a1afb365ff0b7f6692262ba24f
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-26 12:47:48 +0530

    Selecting Element using class, ID and Tag

---
+++ 0021_JavaScript(Part_9)/03_Selecting_Element/app.js
@@ -0,0 +1,47 @@
+// ----------------------------------------------------------------------------
+// For getting element by id
+
+// document.getElementById("mainImg")
+
+// document.all;
+// HTMLAllCollection(38) [html, head, meta, meta, meta, title, link, body, h1, img#mainImg, h2, p, b, a, b, b, a, a, div.box, h4, ul, li, a.boxLink, li, a.boxLink, li, ul, li, a.boxLink, li, a.boxLink, h2, p#description, div.images, img.oldImg, img.oldImg, img.oldImg, script, viewport: meta, mainImg: img#mainImg, description: p#description]
+
+// let myObj1 = document.getElementById("mainImg")
+// undefined
+
+// myObj1
+// <img src=​"assets/​spiderman_img.png" id=​"mainImg">​
+
+// myObj1.src
+// 'file:///Users/kishanagarwal/Documents/Development_Sigma/0021_JavaScript(Part_9)/02_Starter_Code/assets/spiderman_img.png'
+
+// myObj1.src = "assets/creation_1.png"
+// 'assets/creation_1.png'
+
+// ----------------------------------------------------------------------------
+// For getting element by class
+// It returns the elements as a HTML collection or empty collection(if the class not found)
+// can be accessed by index
+// document.getElementsByClassName("oldImg");
+// HTMLCollection(3)[img.oldImg, img.oldImg, img.oldImg]
+// 0: img.oldImg
+// 1: img.oldImg
+// 2: img.oldImg
+// length: 3
+// [[Prototype]]: HTMLCollection
+
+// let smallImg = document.getElementsByClassName("oldImg");
+
+// for(let i = 0;i<smallImg.length;i++){
+//     console.dir(smallImg[i].src);
+// }
+
+// ----------------------------------------------------------------------------
+// For getting element by Tag Name
+// document.getElementsByTagName("p");
+// HTMLCollection(2)[p, p#description, description: p#description]
+// 0: p
+// 1: p#description
+// description: p#description
+// length: 2
+// [[Prototype]]: HTMLCollection
