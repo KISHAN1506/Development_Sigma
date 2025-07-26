9a0693f16c8f90dc4ff3aedbf65fbdae73e9e29b
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-26 13:02:29 +0530

    Query Selector

---
+++ 0021_JavaScript(Part_9)/04_Query_Selector/app.js
@@ -0,0 +1,10 @@
+// Query Selector are very similar to CSS Selectors
+
+// returns the first occurence of the same
+console.dir(document.querySelector("p"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+
+console.dir(document.querySelectorAll("p"));
+console.dir(document.querySelectorAll("#description"));
+console.dir(document.querySelectorAll(".oldImg"));
