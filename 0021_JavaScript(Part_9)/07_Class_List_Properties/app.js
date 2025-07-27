a4fd62fd097efd8795f1ff174a59c718c0d3a382
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-27 10:50:29 +0530

    classList Property

---
+++ 0021_JavaScript(Part_9)/07_Class_List_Properties/app.js
@@ -0,0 +1,24 @@
+// Manipulating Style
+
+// using classList
+
+// obj.classList
+// 	•	classList.add() to add new classes
+// 	•	classList.remove() to remove classes
+// 	•	classList.contains() to check if class exists
+// 	•	classList.toggle() to toggle between add & remove
+
+// let image = document.querySelector("h1")
+// undefined
+// image.setAttribute("class","someActions")
+// undefined
+
+// image.classList.add("magentaaa");
+// undefined
+// image.classList;
+// DOMTokenList(2) ['someActions', 'magentaaa', value: 'someActions magentaaa']
+
+// setAttribute makes the element's only class as the passes class in func
+// while classList.add() --> add with pre existing classes intact
+
+// Also all the changes are displayed in HTML also
