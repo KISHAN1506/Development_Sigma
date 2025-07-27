72eb24c8b8540629f9e82914b012b975efc3bb61
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-27 12:41:26 +0530

    Practice Qn

---
+++ 0021_JavaScript(Part_9)/09_Practice_Qns/app.js
@@ -0,0 +1,29 @@
+// Q1
+let para = document.createElement('p');
+para.innerText = "Hey I am red";
+para.style.color = "red";
+
+document.querySelector("body").append(para);
+
+// Q2
+let heading3 = document.createElement('h3');
+heading3.innerText = "Hey I am blue";
+heading3.style.color = "blue";
+document.body.append(heading3);
+
+// Q3
+
+let divaddn = document.createElement("div");
+divaddn.style.border = "black";
+divaddn.style.backgroundColor = "pink";
+document.querySelector("h3").append(divaddn);
+
+
+let head1 = document.createElement("h1");
+head1.innerHTML = "I am in a div";
+divaddn.appendChild(head1);
+
+let parag = document.createElement("p");
+parag.innerHTML = "Me too!";
+divaddn.appendChild(parag);
+
