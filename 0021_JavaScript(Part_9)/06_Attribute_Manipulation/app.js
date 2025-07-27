e6142cea308413b0d2b7e5c7da2555d8de8a15c4
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-27 10:35:32 +0530

    Attribubte Manipulation

---
+++ 0021_JavaScript(Part_9)/06_Attribute_Manipulation/app.js
@@ -0,0 +1,11 @@
+// Getter and setter
+// let image = document.querySelector("img");
+// undefined
+// image.getAttribute("id");
+// 'mainImg'
+// image.setAttribute("id","Spoooooderman");
+// undefined
+
+// When we access style using document.style
+// It only shows the inline styles that are already defined
+// also the changes made are made as in Inline styling
