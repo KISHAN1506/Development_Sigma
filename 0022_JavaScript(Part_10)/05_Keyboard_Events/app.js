d63143faea68036f63971db54f4eb8a07097b0e4
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-28 23:53:47 +0530

    Keyboard Events

---
+++ 0022_JavaScript(Part_10)/05_Keyboard_Events/app.js
@@ -0,0 +1,9 @@
+let input = document.querySelector("input");
+
+input.addEventListener("keydown",function(event) {
+    console.log("key = ",event.key);
+    console.log("Code = ",event.code);
+
+    console.log("Key was pressed");
+    
+});
