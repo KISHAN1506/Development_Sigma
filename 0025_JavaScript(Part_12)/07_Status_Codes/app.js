ea1d8624419c375e9480a1ca305bf82a469e4512
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-08-05 23:38:30 +0530

    Status Codes

---
+++ 0025_JavaScript(Part_12)/07_Status_Codes/app.js
@@ -0,0 +1,6 @@
+// We have different types of status codes:
+
+// 200 - OK (No problem runned successfully)
+// 404 - Not Found (when no such URL is found when request is sent)
+// 400 - Bad Request
+// 500 - Internal Server Errors
