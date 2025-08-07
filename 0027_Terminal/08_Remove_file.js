84a9d10b387a6d1f946d8e1ad0ca6a553fae7312
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-08-08 00:03:56 +0530

    Remove Files (with Flags)

---
+++ 0027_Terminal/08_Remove_file.js
@@ -0,0 +1,11 @@
+// These commands are very sensitive --> as files get permanently 
+// deleted from everywhere, no way to recover 
+
+// in a director --> rm app.js 
+
+// rm command is to delete files not the whole Folders/Directory
+
+// to remove Directory --> rmdir (removes only empty folder)
+
+// rm -rf :: removes any folder , does not matter if 
+// r--> recursive delete , f--> forced delete (Recursive Force) (remove with force)
