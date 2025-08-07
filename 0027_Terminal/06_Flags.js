62a732f4d7eb4f61ddebab788c9d7c09ad55aeb2
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-08-08 00:03:39 +0530

    Flags

---
+++ 0027_Terminal/06_Flags.js
@@ -0,0 +1,10 @@
+// manual command
+
+// if we want to get details of any of the command write "man ls" or so
+
+// q to get out of manual
+
+// Eg: ls -l : long information 
+// ls -a : to show all the files be it hidden or normal
+
+// ls -la : to combine both the flags 
