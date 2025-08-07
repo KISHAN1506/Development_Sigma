ab657c9e9b6e372a3f5b97c8d06b5bda7f6bc257
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-08-07 23:20:54 +0530

    Navigation in Terminal

---
+++ 0027_Terminal/04_Navigation.js
@@ -0,0 +1,17 @@
+// cd --> change directory
+// cd Desktop
+// kishanagarwal@Kishans-MacBook-Air ~ % cd Desktop
+// kishanagarwal@Kishans-MacBook-Air Desktop % 
+
+// To get out of current working directory --> cd..
+
+// To get in multiple level of directory --> cd Desktop/Kishan
+
+// to go 2 steps backward : cd ../..
+
+// Absolute paths start with /
+// Relative paths start with name then slash "/"
+// Relative will add the path to the current folder and navigate
+
+
+// /--> to move 
