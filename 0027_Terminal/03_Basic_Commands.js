a93290a0dd12472a0ee2d0bef90ad3d3f30098be
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-08-07 22:57:07 +0530

    Basic Commands

---
+++ 0027_Terminal/03_Basic_Commands.js
@@ -0,0 +1,23 @@
+// ls --> list files (show my files)
+// Last login: Sat Jul 26 20:52:01 on ttys092
+// kishanagarwal@Kishans-MacBook-Air ~ % ls
+// Applications				matlab_crash_dump.29385-1
+// Applications (Parallels)		mechvibes_custom
+// db					mechvibes.log
+// Desktop					Movies
+// Documents				Music
+// Downloads				Parallels
+// iCloud Drive (Archive)			Pictures
+// include					Public
+// Kishan 2 Summer Flight Booking.pdf	Templates
+// Library
+
+// ~ --> home directory
+
+// pwd --> print working direction
+// kishanagarwal@Kishans-MacBook-Air ~ % pwd
+// /Users/kishanagarwal
+
+// clear --> clears all the commands available on the screen
+
+// To get old commands --> press upward arrow
