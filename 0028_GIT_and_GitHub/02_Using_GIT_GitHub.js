87c621aaa6e25e76c4d4fcba75fa29c4f419459c
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-09 19:30:56 +0530

    Using GIT and Github

---
+++ 0028_GIT_and_GitHub/02_Using_GIT_GitHub.js
@@ -0,0 +1,32 @@
+// kishanagarwal@Kishans-MacBook-Air ~ % git --version
+// git version 2.39.5 (Apple Git-154) 
+
+// We will use git using command line
+
+// Clone --> to clone our repository in our system
+// Status-->Status of what is the difference between our repo and local system file
+
+// To make changes visible to repo
+
+// "ADD process" take the file from git working directory --> git staging area
+// 1st --> add --> commit --> push
+
+// git init --> to add git to the folder
+
+// git remote add origin <link> --> to tell local system 
+// about where to push the code (ie which repo)
+
+// git remote add origin <- link ->
+// git remote -v
+// (to verify remote)
+// git branch
+// (to check branch)
+
+// git branch -M main (to rename branch)
+// git push origin main
+
+
+//  WORKFLOW:
+
+// Via GitHub : code change --> commit
+// Via Local Repo : code change --> add --> commit --> push
