cead6613b0ce59da7ade5980f7dff2a053642783
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-09 19:31:12 +0530

    Merge Requests

---
+++ 0028_GIT_and_GitHub/04_Merge_Request.js
@@ -0,0 +1,7 @@
+// To merge a new branch in the main branch
+
+
+
+// git pull origin main
+// used to fetch and download content from a remote repo and immediately update the local repo
+// to match that content.
