fe3651c05a7324ae339f2305e405a80f8b1633d3
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-09 19:48:48 +0530

    Forking

---
+++ 0028_GIT_and_GitHub/06_Fork.js
@@ -0,0 +1,10 @@
+// What is Forking?
+// A fork is a new repository that shares code and visibility settings with the original "upstream" repository.
+// Fork is a rough copy.
+
+// To make changes in someone else's repo
+// We have to fork it to our own account then all the changes
+// can be added and pushed
+
+// at the end after adding the code, 
+// we can create a pull request with the actual repo 
