37ac62deb70537f7c4ef10cdf54d099ee53e3a05
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-22 19:56:22 +0530

    Importing Packages instead of require

---
+++ 0029_Backend_1(Node.js)/11_Import.js
@@ -0,0 +1,16 @@
+// import { sum, PI } from "./math";
+
+// console.log(sum(1,2));
+// We have to make a package.json file the the folder 
+
+// and at the end add
+// "type": "module"
+
+
+// require V/S Import
+// import { sum } from "./math.js"
+// We can't selectively load only the pieces we need with require but with import, we can selectively load only the pieces we need, which can save memory.
+// Loading is synchronous for 'require' but can be asynchronous for 'import'.
+
+// import { generate } from "random-words";
+// console.log(generate());
