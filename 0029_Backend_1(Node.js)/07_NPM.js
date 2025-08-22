aaaeeb3bca81c715c2d31719db1cc573c95632e6
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-22 19:38:12 +0530

    NPM

---
+++ 0029_Backend_1(Node.js)/07_NPM.js
@@ -0,0 +1,6 @@
+// NPM --> Node Package Manager
+
+//  NPM can be considered as a library of useful packages like:
+// Express.js, react.js
+
+// It is a command line tool --> to make accessible to other developers
