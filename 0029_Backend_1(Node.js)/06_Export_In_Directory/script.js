4aa5abced38fda26dcab3573fbc75e7301e7f358
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-22 18:53:36 +0530

    Export in Directory

---
+++ 0029_Backend_1(Node.js)/06_Export_In_Directory/script.js
@@ -0,0 +1,13 @@
+const vals = require("./Fruits");
+
+console.log(vals);
+// kishanagarwal@Kishans-MacBook-Air 0029_Backend_1(Node.js) % node 06_Export_In_Directory/script.js
+// [
+//   { name: 'apple', color: 'red' },
+//   { name: 'mango', color: 'yellow' },
+//   { name: 'banana', color: 'yellow' }
+// ]
+
+// index.js is the compulsory name, as it is the entry point,
+// similar as in main in c,cpp,java
+
