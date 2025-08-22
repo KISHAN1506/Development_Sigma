c3a58b51d1ed5fdec7c163918ef855c6661fd7c3
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-22 18:28:26 +0530

    Running JS Files using Node

---
+++ 0029_Backend_1(Node.js)/03_Running_File_Using_Node.js
@@ -0,0 +1,21 @@
+for(let i = 0 ;i<5;i++){
+    console.log("Hello, ",i);
+}
+
+console.log("BYEEEEE!!");
+
+
+// touch to create file and node to run the file
+
+// OUTPUT in Terminal : 
+
+// kishanagarwal@Kishans-MacBook-Air 0029_Backend_1(Node.js) % touch 04_File_Using_Node.js
+// kishanagarwal@Kishans-MacBook-Air 0029_Backend_1(Node.js) % node 04_File_Using_Node.js 
+// Hello,  0
+// Hello,  1
+// Hello,  2
+// Hello,  3
+// Hello,  4
+// BYEEEEE!!
+// kishanagarwal@Kishans-MacBook-Air 0029_Backend_1(Node.js) % 
+
