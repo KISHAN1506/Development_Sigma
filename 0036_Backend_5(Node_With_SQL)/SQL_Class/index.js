f1327a57baf03c1d5a0e533fc6af336c6d4a1ac4
Kishan Agarwal <kishanagarwal1506@gmail.com>
2025-08-31 23:35:05 +0530

    Final Addition of all Routes

--- 0036_Backend_5(Node_With_SQL)/SQL_Class/index.js
+++ 0036_Backend_5(Node_With_SQL)/SQL_Class/index.js
@@ -3,9 +3,12 @@ const mysql = require('mysql2');
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
 const port = 8080;
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"));
 
@@ -46,6 +49,51 @@ app.get("/user", (req, res) => {
         res.send("Some error found in DataBase!!");
     }
 })
+
+// EDIT ROUTE
+app.get("/user/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let q = `SELECT * from user where id="${id}"`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", { user });
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("Some error found in DataBase!!");
+    }
+})
+
+
+// UPDATE (DB) Route
+app.patch("/user/:id", (req, res) => {
+    let { id } = req.params;
+    let { password: formPass, username: newUsername } = req.body;
+    let q = `SELECT * from user where id="${id}"`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if (formPass != user.password) {
+                res.send("Wrong Password");
+            } else {
+                let q2 = `UPDATE user SET username="${newUsername}" where id="${id}"`;
+                connection.query(q2, (err2, result) => {
+                    if(err2) throw err2;
+                    console.log(err2);
+                    res.redirect("/user");
+                })
+            }
+
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("Some error found in DataBase!!");
+    }
+})
+
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
