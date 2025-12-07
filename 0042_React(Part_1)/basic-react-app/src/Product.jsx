4c2ecf335e7346ebaf308ccad74e8d80ab3d1e81
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-12-07 14:44:10 +0530

    React conditionals and styling

--- 0042_React(Part_1)/basic-react-app/src/Product.jsx
+++ 0042_React(Part_1)/basic-react-app/src/Product.jsx
@@ -1,12 +1,37 @@
-import "./Product.css"
+import "./Product.css";
 
-function Product({ itemName, price }) {
+function Product({ itemName, price = 1 }) {
+  // M1 to give discount
+  // if (price > 30000) {
+  //   return (
+  //     <div className="maindiv">
+  //       <p>{itemName}</p>
+  //       <h3>price : {price}</h3>
+  //       <p>5% Discount</p>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div className="maindiv">
+  //       <p>{itemName}</p>
+  //       <h3>price : {price}</h3>
+  //     </div>
+  //   );
+  // }
+
+  // Method-2
+  let isDiscount = price>30000;
+  let styles = { backgroundColor: isDiscount ? "lightblue" : "" };
   return (
-    <div className="maindiv">
+    <div className="maindiv" style={styles}>
       <p>{itemName}</p>
       <h3>price : {price}</h3>
+      {isDiscount && <p>5%</p>}
     </div>
   );
+}
+{
+  /* {price > 30000 ? <p>"5%"</p> : null} */
 }
 
 export default Product;
--- 0042_React(Part_1)/basic-react-app/src/ProductTab.jsx
+++ 0042_React(Part_1)/basic-react-app/src/ProductTab.jsx
@@ -1,11 +1,17 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+  let features = ["fast", "Smooth", "Big battery"];
+  // let features = {
+  //   a: "High-Tech",
+  //   b: "Durable",
+  //   c: "Fast",
+  // };
   return (
     <div>
-      <Product itemName="Mobile" price={10000}></Product>
+      <Product itemName="Mobile" price={10000} features={features}></Product>
       <Product itemName="Laptop" price={40000}></Product>
-      <Product itemName="Camera" price={5000}></Product>
+      <Product itemName="Camera" price={1}></Product>
     </div>
   );
 }
