91c988086a073fcd3a508258e72604ab27dbc00b
KISHAN1506 <kishanagarwal1506@gmail.com>
2026-06-15 15:50:31 +0530

    Added image preview in edit functionality with compressed image to handle system load

--- 0040_WanderLust_Major_Project/controllers/listings.js
+++ 0040_WanderLust_Major_Project/controllers/listings.js
@@ -37,15 +37,24 @@ module.exports.renderEditForm = async (req, res) => {
     const listing = await Listing.findById(id);
     if(!listing){
         req.flash("error","Listing does not exist!")
-        res.redirect("/listings")
-    }else{
-        res.render("listings/edit.ejs", { listing })
+        return res.redirect("/listings")
     }
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload","/upload/w_250")
+    res.render("listings/edit.ejs", { listing ,originalImageUrl})
 }
 
 module.exports.updateListing = async (req, res) => {
     let id = req.params.id;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    if(typeof req.file !== "undefined"){
+        let url = req.file.path;
+        let filename = req.file.filename;
+    
+        listing.image = {url,filename};
+        await listing.save();
+    }
     req.flash("success","Listing Updated!")
     res.redirect(`/listings/${id}`);
 }
